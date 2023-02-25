3b2a085aa9ea93e3d5c9cead42533c06d896f82b
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-02-25 13:38:32 +0100

    Update App with components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Feedback } from "./Feedback/Feedback";
+import { Statistics } from "./Statistics/Statistics";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <Feedback />
+      <Statistics />
     </div>
   );
 };
