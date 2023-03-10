0d00b2f1c726c952b7554b6f2beab12d24b2d55d
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2023-03-10 17:57:41 +0100

    Refactor

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,23 +1,36 @@
 import { Component } from 'react';
 import css from './FeedbackOptions.module.css';
+import { capitalizeFirstLetter } from 'utils/functions';
+import PropTypes from 'prop-types';
 
 export class FeedbackOptions extends Component {
   render() {
-    const {  onLeaveFeedback } = this.props;
+    const { onLeaveFeedback, scoreKeys } = this.props;
     return (
       <>
-        <h1>Espresso Coffee</h1>
         <p>asks for your feedback!</p>
-        <button className={css.button} type="button" onClick={onLeaveFeedback}>
-          Positive
-        </button>
-        <button className={css.button} type="button" onClick={onLeaveFeedback}>
-          Neutral
-        </button>
-        <button className={css.button} type="button" onClick={onLeaveFeedback}>
-          Negative
-        </button>
+        {scoreKeys &&
+          scoreKeys.length &&
+          scoreKeys.map(scoreKey => (
+            <button
+              key={scoreKey}
+              className={css.button}
+              type="button"
+              onClick={onLeaveFeedback}
+            >
+              {capitalizeFirstLetter(scoreKey)}
+            </button>
+          ))}
       </>
     );
   }
-}
+};
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func,
+  scoreKeys: PropTypes.array.isRequired,
+};
+
+
+
+
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import css from './Statistics.module.css';
+import { capitalizeFirstLetter } from 'utils/functions';
+import PropTypes from 'prop-types';
 
 export class Statistics extends Component {
-
   render() {
-    const { positive, negative, neutral, total, positiveFeedback } = this.props;
+    const { total, positiveFeedback, scoreKeys } = this.props;
     return (
       <div className={css.stats}>
-        <h2>Stats</h2>
-        <p>Positive: {positive}</p>
-        <p>Neutral: {neutral}</p>
-            <p>Negative: {negative}</p>
+        {scoreKeys &&
+          scoreKeys.length &&
+          scoreKeys.map(scoreKey => (
+            <p key={scoreKey}>
+              {capitalizeFirstLetter(scoreKey)}: {this.props[scoreKey]}
+            </p>
+          ))}
         <p>Total: {total}</p>
         <p>Positive feedback: {positiveFeedback}</p>
       </div>
     );
   }
+}
+
+Statistics.propTypes = {
+  total: PropTypes.number,
+  positiveFeedback: PropTypes.number,
+  scoreKeys: PropTypes.array.isRequired,
 }
